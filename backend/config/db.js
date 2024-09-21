84b45babfbca346c41833fa1ec12befee411e9ce
sparky17 <vghanshyam044@gmail.com>
2024-09-21 23:36:52 +0530

    a

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,9 +6,12 @@ dotenv.config();
 const connectDB = async ()=>{
     try{
         const conn=await mongoose.connect(process.env.MONGO_URI)
-        console.log('\x1b[32m%s\x1b[0m',`Mongo db is connected at ${process.env.MONGO_URI}`)
+        console.log('\x1b[32m%s\x1b[0m',`Mongo db is connected at ${process.env.MONGO_URI}`,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+        })
        }catch(error){
-            console.error(`Error ${error}`)
+            console.error('\x1b[31m%s\x1b[0m', `Error: ${error.message}`);
        }
 }   
 module.exports=connectDB;
---
+++ backend/model/Playlist.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const Playlist= new mongoose.Schema({
+   name:{
+    type:String,
+    required:true,
+   },
+   thumbnail:{
+    type:String,
+    required:true,
+   },
+   owner:{
+       type:mongoose.type.ObjectId,
+       ref:"user"
+    },
+    songs:[{
+     type:mongoose.Types.ObjectId,
+     ref:"song",
+    }],
+    collaborators:[{
+        type:mongoose.Types.ObjectId,
+        ref:"user",
+    }]
+
+});
+
+const PlaylistModel =mongoose.model("Playlist",Playlist);
+module.exports=PlaylistModel;
