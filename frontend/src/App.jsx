df65beaa626e24b5b373f4e57555f78afa1c1395
sparky17 <vghanshyam044@gmail.com>
2024-09-29 23:38:36 +0530

    Code cleaning in frontEnd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./Components/Home/Home";
 
 export default function App() {
   console.log("from app");
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <h1 className="text-3xl  font-bold underline">
+    Hello world!
+  </h1>
   );
 }
